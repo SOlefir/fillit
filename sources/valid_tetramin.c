5837c6aae3b1c4c7e7ca55695a1e4de70095454e
Sofiaa OLEFIR <solefir@e1r10p10.unit.ua>
2019-04-18 00:55:06 +0300

    comments

--- sources/valid_tetramin.c
+++ sources/valid_tetramin.c
@@ -6,7 +6,7 @@
 /*   By: solefir <solefir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 21:30:34 by solefir           #+#    #+#             */
-/*   Updated: 2019/04/18 00:35:13 by solefir          ###   ########.fr       */
+/*   Updated: 2019/04/18 00:54:38 by solefir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,12 @@ int		valid_tetraminos(char *input, int len)
 	int	n;
 	int dot;
 
-	hash = count_char(input, len, '#') % 4;
+	hash = count_char(input, len, '#') % 4; 
 	n = (count_char(input, len, '\n') + 2) % 5;
 	dot = count_char(input, len, '.') % 3;
-	if ((dot + hash + n) != 0)
+	if ((dot + hash + n) != 0) //опред. валидное кол-во символов
 		return (-1);
-	if (valid_form(input) < 0)
+	if (valid_form(input) < 0) // правильная ли форма фигуры
 		return (-1);
 	return (1);
 }
